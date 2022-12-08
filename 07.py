afd8595b24906a32246fa189d033c02cb47684df
sve <>
2022-12-08 08:03:14 +0200

    update 7

--- 07.py
+++ 07.py
@@ -22,13 +22,12 @@ def construct_tree(index, node):
         return
 
     line = lines[index]
-    if line.startswith('$ cd'):
-        if line.endswith('..'):
-            construct_tree(index + 1, node.parent)
-        else:
-            child = Folder(0, [], node)
-            node.children.append(child)
-            construct_tree(index + 1, child)
+    if line.startswith('$ cd ..'):
+        construct_tree(index + 1, node.parent)
+    elif line.startswith('$ cd'):
+        child = Folder(0, [], node)
+        node.children.append(child)
+        construct_tree(index + 1, child)
     elif line.startswith('$ ls'):
         index += 1
         while index < len(lines) and not lines[index].startswith('$'):
@@ -58,8 +57,6 @@ def get_folder_sizes(node):
 root = Folder(0, [], None)
 construct_tree(1, root)
 calculate_folder_size(root)
-
-
 folder_sizes = get_folder_sizes(root)
 print(sum(folder_size for folder_size in folder_sizes if folder_size <= 100000))
 print(
