d86a0a01a7a719263b7c3ac2f400c03747fb24e4
sve <>
2022-12-10 09:24:52 +0200

    refactor

--- 10.py
+++ 10.py
@@ -1,37 +1,22 @@
 from utils import get_input
 
-text = get_input(day=10, year=2022)
+text = get_input(day=10, year=2022, test=False, test_index=1)
 lines = text.splitlines()
 
 
-def part1():
-    x = 1
-    xs = [0]
-    for instruction in text.splitlines():
+x = 1
+xs = []
+for instruction in text.splitlines():
+    xs.append(x)
+    if instruction.startswith('addx'):
         xs.append(x)
-        if instruction.startswith('addx'):
-            xs.append(x)
-            x += int(instruction.split()[1])
-    return sum(cycle * xs[cycle] for cycle in range(20, len(xs), 40))
+        x += int(instruction.split()[1])
 
+image = ''
+for crt in range(240):
+    if crt % 40 == 0 and image:
+        image += '\n'
+    image += '#' if abs(xs[crt] - (crt % 40)) <= 1 else '.'
 
-def part2():
-    x = 1
-    image = ''
-    instructions = lines[::-1]
-    stack = instructions.pop().split()
-    for crt in range(240):
-        crt %= 40
-        if crt == 0 and image:
-            image += '\n'
-        image += '#' if abs(x - crt) <= 1 else '.'
-        instruction = stack.pop(0)
-        if instruction not in ['noop', 'addx']:
-            x += int(instruction)
-        if len(stack) == 0 and len(instructions) > 0:
-            stack = instructions.pop().split()
-    return image
-
-
-print(part1())
-print(part2())
+print(sum((cycle + 1) * xs[cycle] for cycle in range(19, len(xs), 40)))
+print(image)
