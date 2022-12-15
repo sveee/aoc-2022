2620fb615531ff1fae6904dde25ea972f9e97e1c
sve <>
2022-12-15 10:35:01 +0200

    refactor day 15

--- 15.py
+++ 15.py
@@ -2,15 +2,15 @@ import re
 
 from utils import get_input
 
-text = get_input(day=15, year=2022)
 
-
-def get_impossible_range(s, b, y):
-    m = abs(s[0] - b[0]) + abs(s[1] - b[1])
-    rx, ry = (s[0] - m + abs(s[1] - y), s[0] + m - abs(s[1] - y))
-    if ry >= rx:
-        return rx, ry
-    return None
+def get_impossible_range(sensor, beacon, y):
+    manhattan = abs(sensor[0] - beacon[0]) + abs(sensor[1] - beacon[1])
+    range_x, range_y = (
+        sensor[0] - manhattan + abs(sensor[1] - y),
+        sensor[0] + manhattan - abs(sensor[1] - y),
+    )
+    if range_y >= range_x:
+        return range_x, range_y
 
 
 def do_intersect(left, right):
@@ -18,12 +18,13 @@ def do_intersect(left, right):
 
 
 def get_impossible_ranges(y):
-    range_set = [
-        r
-        for sensor, beacon in sensor_and_beacons
-        if (r := get_impossible_range(sensor, beacon, y))
-    ]
-    return union(range_set)
+    return union(
+        [
+            impossible_range
+            for sensor, beacon in sensor_and_beacons
+            if (impossible_range := get_impossible_range(sensor, beacon, y))
+        ]
+    )
 
 
 def union(range_set):
@@ -31,31 +32,33 @@ def union(range_set):
     left = range_set[0]
     result = []
     for right in range_set[1:]:
-        if not do_intersect(left, right):
+        if do_intersect(left, right):
+            left = min(left[0], right[0]), max(left[1], right[1])
+        else:
             result.append(left)
             left = right
-        else:
-            left = min(left[0], right[0]), max(left[1], right[1])
     result.append(left)
     return result
 
 
-y = 2000000
-max_y = 4000000
+text = get_input(day=15, year=2022)
 sensor_and_beacons = []
 for line in text.splitlines():
-    s_x, s_y, b_x, b_y = map(int, re.findall('(-?\d+)', line))
-    sensor_and_beacons.append(((s_x, s_y), (b_x, b_y)))
+    sensor_x, sensor_y, beacon_x, beacon_y = map(int, re.findall('(-?\d+)', line))
+    sensor_and_beacons.append(((sensor_x, sensor_y), (beacon_x, beacon_y)))
 
-
+# part 1
+y = 2000000
 impossible_range = get_impossible_ranges(y)[0]
 beacon_xs_in_range = {
     beacon[0]
-    for _s, beacon in sensor_and_beacons
+    for _sensor, beacon in sensor_and_beacons
     if impossible_range[0] <= beacon[0] <= impossible_range[1] and beacon[1] == y
 }
 print(impossible_range[1] - impossible_range[0] + 1 - len(beacon_xs_in_range))
 
+# part 2
+max_y = 4000000
 for y in range(max_y + 1):
     range_set = get_impossible_ranges(y)
     if len(range_set) > 1:
