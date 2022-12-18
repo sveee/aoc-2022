aebabdb0b532009ab7f147e769674fc174b42a4d
sve <>
2022-12-18 10:31:29 +0200

    add day 18

--- 18.py
+++ 18.py
@@ -1,9 +1,6 @@
-from collections import Counter, defaultdict
-from itertools import combinations
-
 from utils import get_input
 
-text = get_input(day=18, year=2022, test=False)
+text = get_input(day=18, year=2022)
 
 directions = [
     (1, 0, 0),
@@ -19,16 +16,35 @@ def get_neighbours(position):
     return [
         tuple((position[k] + direction[k] for k in range(3)))
         for direction in directions
+        if all(-2 <= position[k] + direction[k] <= 21 for k in range(3))
     ]
 
 
-cube_positions = set()
+def find_air(position):
+    stack = [position]
+    air = set()
+    while len(stack) > 0:
+        position = stack.pop()
+        if position in air or position in droplet:
+            continue
+        air.add(position)
+        for neighbour in get_neighbours(position):
+            stack.append(neighbour)
+    return air
+
+
+droplet = set()
 for line in text.splitlines():
-    cube_positions.add(tuple(map(int, line.split(','))))
+    droplet.add(tuple(map(int, line.split(','))))
 
-area = 0
-for posiiton in cube_positions:
+
+air = find_air((20, 20, 20))
+surface_area1 = 0
+surface_area2 = 0
+for posiiton in droplet:
     for neighbour in get_neighbours(posiiton):
-        area += neighbour not in cube_positions
+        surface_area1 += neighbour not in droplet
+        surface_area2 += neighbour in air
 
-print(area)
+print(surface_area1)
+print(surface_area2)
