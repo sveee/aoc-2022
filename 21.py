f4548ee8a73f20f01033f8b634faee5766f37d8b
sve <>
2022-12-22 22:05:01 +0200

    add day 21

--- 21.py
+++ 21.py
@@ -4,7 +4,8 @@ from typing import Optional
 
 from utils import get_input
 
-text = get_input(day=21, year=2022, test=False)
+text = get_input(day=21, year=2022)
+lines = text.splitlines()
 
 
 @dataclass
@@ -35,20 +36,47 @@ class Node:
         return self.value
 
 
-node_by_name = {}
-for line in text.splitlines():
-    name, right = line.split(':')
+def construct_compute_graph(humn=None):
+    node_by_name = {}
+    for line in lines:
+        name, right = line.split(':')
 
-    if match := re.search(' ([a-z]+) ([\+\-\*\/]) ([a-z]+)', line):
-        node = node_by_name.setdefault(name, Node())
-        node.left = node_by_name.setdefault(match.group(1), Node())
-        node.right = node_by_name.setdefault(match.group(3), Node())
-        node.operation = match.group(2)
+        if match := re.search(' ([a-z]+) ([\+\-\*\/]) ([a-z]+)', right):
+            node = node_by_name.setdefault(name, Node())
+            node.left = node_by_name.setdefault(match.group(1), Node())
+            node.right = node_by_name.setdefault(match.group(3), Node())
+            node.operation = match.group(2)
 
-    elif match := re.search(' (\d+)', line):
-        node = node_by_name.setdefault(name, Node())
-        node.value = int(match.group(1))
-    else:
-        raise ValueError(f'Unknown pattern {line}')
+        elif match := re.search(' (\d+)', right):
+            node = node_by_name.setdefault(name, Node())
+            value = humn if humn is not None and name == 'humn' else int(match.group(1))
+            node.value = value
+        else:
+            raise ValueError(f'Unknown pattern {right}')
+    return node_by_name
 
-print(node_by_name['root'].compute())
+
+def root_children_difference(humn):
+    graph = construct_compute_graph(humn)
+    root = graph['root']
+    return root.left.compute() - root.right.compute()
+
+
+def sign(v):
+    return -1 if v < 0 else 0 if v == 0 else 1
+
+
+def binary_search(left, right):
+    right_sign = sign(root_children_difference(right))
+    while left + 1 < right:
+        mid = (left + right) // 2
+        value = root_children_difference(mid)
+        if value == 0 or sign(value) == right_sign:
+            right = mid
+        else:
+            left = mid
+    return right
+
+
+print(construct_compute_graph()['root'].compute())
+print(binary_search(1, 10**20))
